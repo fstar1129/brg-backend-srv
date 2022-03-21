22a17cc30a07994ed34ab9faa347788e1ee6d88f
aditya.rout <aditya.rout@latoken.com>
2022-03-21 18:19:31 +0530

    working version

--- src/config/viper.go
+++ src/config/viper.go
@@ -32,13 +32,13 @@ type viperConfig struct {
 
 func (v *viperConfig) Init() {
 	viper.AutomaticEnv()
-	viper.AddConfigPath(os.Getenv("FILE_PATH"))
-	// viper.AddConfigPath(".")
+	// viper.AddConfigPath(os.Getenv("FILE_PATH"))
+	viper.AddConfigPath("../config-files")
 	replacer := strings.NewReplacer(`.`, `_`)
 	viper.SetEnvKeyReplacer(replacer)
 	viper.SetConfigType(`json`)
-	viper.SetConfigName(os.Getenv("FILE_NAME"))
-	// viper.SetConfigFile(`config.json`)
+	// viper.SetConfigName(os.Getenv("FILE_NAME"))
+	viper.SetConfigName(`config.json`)
 	if _, err := os.Stat("./config.json.local"); !os.IsNotExist(err) {
 		viper.SetConfigFile(`config.json.local`)
 	}
