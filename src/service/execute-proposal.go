7b1194555756512f7a57f1ce8a3e77bcab21bb44
aditya.rout <aditya.rout@latoken.com>
2022-05-02 17:46:59 +0530

    with tx checking

--- src/service/execute-proposal.go
+++ src/service/execute-proposal.go
@@ -42,15 +42,15 @@ func (r *BridgeSRV) sendExecuteProposal(worker workers.IWorker, event *storage.E
 	r.logger.Infof("Execute parameters:  depositNonce(%d) | sender(%s) | outAmount(%s) | resourceID(%s) | chainID(%s)\n",
 		event.DepositNonce, event.ReceiverAddr, event.OutAmount, event.ResourceID, worker.GetChainName())
 	if worker.GetChainName() == "LA" {
-		// if event.ResourceID == r.storage.FetchResourceIDByName("amToken").ID {
-		// 	wor := r.Workers["POS"]
-		// 	liquidity, _ := wor.GetLiquidityIndex(wor.GetConfig().AmTokenHandlerAddress, wor.GetConfig().AMUSDTContractAddr)
-		// 	txHash, err = worker.ExecuteProposalLa(event.DepositNonce, utils.StringToBytes8(event.OriginChainID), utils.StringToBytes8(event.DestinationChainID), utils.StringToBytes32(event.ResourceID),
-		// 		event.ReceiverAddr, event.OutAmount, liquidity)
-		// } else {
-		txHash, err = worker.ExecuteProposalLa(event.DepositNonce, utils.StringToBytes8(event.OriginChainID), utils.StringToBytes8(event.DestinationChainID), utils.StringToBytes32(event.ResourceID),
-			event.ReceiverAddr, event.OutAmount, nil)
-		// }
+		if event.ResourceID == r.storage.FetchResourceIDByName("amToken").ID {
+			wor := r.Workers["POS"]
+			liquidity, _ := wor.GetLiquidityIndex(wor.GetConfig().AmTokenHandlerAddress, wor.GetConfig().AMUSDTContractAddr)
+			txHash, err = worker.ExecuteProposalLa(event.DepositNonce, utils.StringToBytes8(event.OriginChainID), utils.StringToBytes8(event.DestinationChainID), utils.StringToBytes32(event.ResourceID),
+				event.ReceiverAddr, event.OutAmount, liquidity)
+		} else {
+			txHash, err = worker.ExecuteProposalLa(event.DepositNonce, utils.StringToBytes8(event.OriginChainID), utils.StringToBytes8(event.DestinationChainID), utils.StringToBytes32(event.ResourceID),
+				event.ReceiverAddr, event.OutAmount, nil)
+		}
 	} else {
 		txHash, err = worker.ExecuteProposalEth(event.DepositNonce, utils.StringToBytes8(event.OriginChainID), utils.StringToBytes8(event.DestinationChainID), utils.StringToBytes32(event.ResourceID),
 			event.ReceiverAddr, event.OutAmount)
